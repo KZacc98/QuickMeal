5bf41fe44ca9b04c9a83eb7e10de4f8dcf7c06f7
Kamil Zachara <kamil.zachara98@gmail.com>
2024-12-04 01:27:49 +0100

    Initial Commit

---
+++ QuickMeal/QuickMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickMealApp.swift
+//  QuickMeal
+//
+//  Created by Kamil Zachara on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuickMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
