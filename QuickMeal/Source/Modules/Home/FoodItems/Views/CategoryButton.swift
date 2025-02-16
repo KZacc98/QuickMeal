5f4b6b5542249bfed21a11e29bbfaa9bf5257b04
Kamil Zachara <kamil.zachara98@gmail.com>
2025-02-16 15:32:58 +0100

    Added top tab bar(WIP), refactored pagerView, started to document views, playing around with haptics.

---
+++ QuickMeal/Source/Modules/Home/FoodItems/Views/CategoryButton.swift
@@ -0,0 +1,42 @@
+//
+//  CategoryButton.swift
+//  QuickMeal
+//
+//  Created by Kamil Zachara on 16/02/2025.
+//
+
+import SwiftUI
+
+/**
+ A button representing a single category in the `CategoriesBar`.
+
+ - Parameters:
+    - category: The `FoodCategory` model representing the category to be displayed.
+    - isSelected: A Boolean value indicating whether this category is currently selected.
+    - onSelect: A closure to be executed when the category is tapped. This typically updates the selected category in the parent view.
+
+ - Note: The button uses a system image for the category. If the category's image is `nil`, the button will not be displayed.
+
+ - SeeAlso: `CategoriesBar`
+ */
+struct CategoryButton: View {
+    var category: FoodCategory
+    var isSelected: Bool
+    var onSelect: () -> Void
+    
+    var body: some View {
+        if let image = category.image {
+            Image(systemName: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .background {
+                    if isSelected {
+                        Color.red
+                    }
+                }
+                .onTapGesture {
+                    onSelect()
+                }
+        }
+    }
+}
--- QuickMeal/Source/Modules/Home/FoodItemsPager/FoodItemsPager.swift
+++ QuickMeal/Source/Modules/Home/FoodItemsPager/FoodItemsPager.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FoodItemsPager: View {
     @StateObject var viewModel: FoodItemsPagerViewModel
     @EnvironmentObject var coordinator: Coordinator
+    @State var selectedCategoryId: String? = nil
     
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \FoodCategory.id, ascending: true)],
@@ -18,32 +19,61 @@ struct FoodItemsPager: View {
     
     var body: some View {
         GeometryReader { geometry in
-            ScrollView(.horizontal) {
-                LazyHStack {
-                    ForEach(foodCategories) { category in
-                        FoodItemsView(viewModel: FoodItemsViewModel(category: category), onItemSelected: { item in
-                            viewModel.manageFoodItems(item)
-                        })
-                        .frame(width: geometry.size.width)
-                    }
-                }
-                .scrollTargetLayout()
-            }
-            .scrollTargetBehavior(.viewAligned)
-            .scrollIndicators(.never)
-            .overlay {
-                if viewModel.hideButton == false {
-                    VStack {
-                        Spacer()
-                        MakeRecipeButton(requiredCount: 3, currentCount: viewModel.foodItems.count) {
-//                            viewModel.makeRecipe()
-                            coordinator.presentSheet(.test)
+            VStack {
+                CategoriesBar(
+                    geometry: geometry,
+                    foodCategories: foodCategories,
+                    selectedCategoryId: $selectedCategoryId
+                )
+                
+                ScrollView(.horizontal) {
+                    LazyHStack {
+                        ForEach(foodCategories) { category in
+                            FoodItemsView(
+                                viewModel: FoodItemsViewModel(category: category),
+                                onItemSelected: { item in
+                                    triggerHapticFeedback(style: .medium)
+                                    viewModel.manageFoodItems(item)
+                                }
+                            )
+                            .frame(width: geometry.size.width)
+                            .id(category.id)
                         }
-                        .frame(height: geometry.size.height * 0.08)
-                        .padding()
+                    }
+                    .scrollTargetLayout()
+                }
+                .scrollPosition(id: $selectedCategoryId)
+                .scrollTargetBehavior(.viewAligned)
+                .scrollIndicators(.never)
+                .overlay {
+                    if viewModel.hideButton == false {
+                        VStack {
+                            Spacer()
+                            MakeRecipeButton(
+                                requiredCount: 3,
+                                currentCount: viewModel.foodItems.count
+                            ) {
+                                coordinator.presentSheet(.test)
+                            }
+                            .frame(height: geometry.size.height * 0.08)
+                            .padding()
+                        }
                     }
                 }
             }
         }
+        .onChange(of: selectedCategoryId) { _, newValue in
+            triggerHapticFeedback()
+        }
+        .onAppear {
+            if let firstCategoryId = foodCategories.first?.id {
+                selectedCategoryId = firstCategoryId
+            }
+        }
+    }
+    
+    private func triggerHapticFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle = .soft) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
     }
 }
